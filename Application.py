56e67f39ac2ae86599a00da40fac81f6d29efaba
Dhafny Buenafe <157882179+dsbuenafe@users.noreply.github.com>
2024-05-18 20:21:12 +0800

    Update Application.py

--- Application.py
+++ Application.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
-import joblib
-import numpy as np
 
 # Load the trained LSTM model
 model_path = 'water_consumption_lstm_model.h5'  # Update with your model path
@@ -11,20 +10,13 @@ model = load_model(model_path)
 
 # Load the scaler
 scaler_path = 'scaler.pkl'  # Update with your scaler path
-scaler = joblib.load(scaler_path)
+scaler = MinMaxScaler()
 
 # Function to preprocess data and make predictions
 def predict_water_consumption(input_data):
-    # Ensure input_data is a 2D array
-    if input_data.ndim == 1:
-        input_data = input_data.reshape(-1, 1)
-    # Scale input data
     input_data_scaled = scaler.transform(input_data)
-    # Reshape input data for LSTM model
-    input_data_reshaped = np.reshape(input_data_scaled, (1, input_data_scaled.shape[0], input_data_scaled.shape[1]))
-    # Make prediction
+    input_data_reshaped = np.reshape(input_data_scaled, (1, input_data_scaled.shape[0], 1))
     predicted_consumption_scaled = model.predict(input_data_reshaped)
-    # Inverse transform the predicted values
     predicted_consumption = scaler.inverse_transform(predicted_consumption_scaled)
     return predicted_consumption
 
@@ -53,8 +45,8 @@ if uploaded_file is not None:
     st.write(latest_data)
 
     # Predict water consumption
-    predicted_consumption = predict_water_consumption(latest_data.values)
+    predicted_values = predict_water_consumption(latest_data.values)
     
     # Show the predicted consumption
     st.subheader('Predicted Water Consumption for the Next Day')
-    st.write(predicted_consumption)
+    st.write(predicted_values)
