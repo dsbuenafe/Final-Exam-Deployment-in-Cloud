39516ef18a257c37212660a8c34e703d2a55b635
Dhafny Buenafe <157882179+dsbuenafe@users.noreply.github.com>
2024-05-18 19:22:53 +0800

    Update Application.py

--- Application.py
+++ Application.py
@@ -3,40 +3,29 @@ import pandas as pd
 import numpy as np
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
+import joblib
 
-# Load the trained model
 model_path = 'water_consumption_lstm_model.h5'
 model = load_model(model_path)
 
-# Function to preprocess user input
-def preprocess_input(user_input, scaler):
-    scaled_input = scaler.transform(user_input)
+scaler_path = 'scaler.save'
+scaler = joblib.load(scaler_path)
 
-    return scaled_input
+
+def preprocess_input(user_input, scaler):
+
+    user_input_scaled = scaler.transform(user_input)
+    return user_input_scaled
 
 def main():
     st.title("Water Consumption Prediction")
-
-    # Load your dataset for reference
-    # Replace 'your_dataset.csv' with the path to your dataset
     df = pd.read_csv('water_consumption.csv')
-
-    # Assuming 'Date' is one of the columns for user input
     user_input_date = st.date_input("Select a date:", value=pd.to_datetime('today'))
-
-    # Additional input fields can be added based on your dataset columns
-
-    # Preprocess the user input
     user_input = pd.DataFrame({'Date': [user_input_date]})
-    scaler = MinMaxScaler(feature_range=(0, 1))  # Use the same scaler as used during training
+    user_input['Date'] = pd.to_datetime(user_input['Date']).map(pd.Timestamp.toordinal)  # Example of processing date
     scaled_input = preprocess_input(user_input, scaler)
-
-    # Predict water consumption
     prediction = model.predict(np.reshape(scaled_input, (1, scaled_input.shape[0], scaled_input.shape[1])))
-
-    # Inverse transform the prediction to get the actual consumption value
     predicted_consumption = scaler.inverse_transform(prediction.reshape(-1, 1))
-
     st.subheader("Predicted Water Consumption:")
     st.write(predicted_consumption[0][0])
 
